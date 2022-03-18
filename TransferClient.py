7fbc18394989c3dc135ac5be8fc59859b10a9a8a
nf255 <92917632+nf255@users.noreply.github.com>
2022-03-18 23:30:00 +0100

    Include matplotlib
    - scatter plot for "time" and "rssi" with matplotlip

--- TransferClient.py
+++ TransferClient.py
@@ -1,5 +1,7 @@
 import socket
 from tkinter import *
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 # Dokumente:
@@ -9,34 +11,6 @@ from tkinter import *
 
 
 def main():
-    root = Tk()
-    root.title("KBRP V2 Test")
-
-    global s
-    s = socket.socket()
-    buffer = 16384
-    connection_status = 1
-
-    # Creating a Label Widget and a button
-    global connection_status_label, connect_button, disconnect_button, button_SyncGetEPCs, label_SyncGetEPCs
-    reader_ip_label = Label(root, text="Reader IP:")
-    reader_ip_entry = Entry(root, width=20)
-    connect_button = Button(root, text="Connect", command=lambda: clickConnect(reader_ip_entry.get()))
-    disconnect_button = Button(root, text="Disconnect", command=lambda: [s.close(), connection_status_label.config(
-        text="Disconnected!", fg='#ff0000')], state=DISABLED)
-    connection_status_label = Label(root, text="")
-    button_SyncGetEPCs = Button(root, text="SyncGetEPCs", command=SyncGetEPCs, state=DISABLED)
-    label_SyncGetEPCs = Label(root, text="")
-
-    # Shoving elements onto the screen
-    reader_ip_label.grid(row=0, column=0, padx=10, pady=10)
-    reader_ip_entry.grid(row=0, column=1, padx=10, pady=10)
-    connect_button.grid(row=0, column=2, padx=10, pady=10)
-    disconnect_button.grid(row=0, column=3, padx=10, pady=10)
-    connection_status_label.grid(row=1, column=0, columnspan=4)
-    button_SyncGetEPCs.grid(row=2, column=0, padx=10, pady=10)
-    label_SyncGetEPCs.grid(row=3, column=0, columnspan=4)
-
     root.mainloop()
 
 
@@ -56,7 +30,17 @@ def SyncGetEPCs():
     message = bytes(enc)
     s.send(message)
     response = s.recv(buffer)
-    label_SyncGetEPCs.config(text=createResponseOutput(response))
+    output_data = createResponseOutput(response)
+    x_plot = np.array([])
+    y_plot = np.array([])
+    for tag in output_data:
+        if tag["type"] == "tag":
+            x_plot = np.append(x_plot, tag["time"])
+            y_plot = np.append(y_plot, tag["rssi"])
+    plt.scatter(x_plot, y_plot)
+    plt.xlabel("Timestamp")
+    plt.ylabel("RSSI Value")
+    plt.show()
 
 
 def createResponseOutput(response):
@@ -74,7 +58,7 @@ def createResponseOutput(response):
 
 def rSyncGetEPCs(c_data):
     tag_list = []
-    info = {"command":"SyncGetEPCs"}
+    info = {"type": "info", "command": "SyncGetEPCs"}
     if c_data[0] == "0x00":
         info["error"] = "none"
     elif c_data[0] == "0x0A":
@@ -86,35 +70,66 @@ def rSyncGetEPCs(c_data):
     e_flag = int(c_data[0], base=16)
     if c_out["error"] == "none":
         while True:
-            extended_result = {}
-            if e_flag == (e_flag | 2**0):
+            extended_result = {"type": "tag"}
+            if e_flag == (e_flag | 2 ** 0):
                 extended_result["port"] = c_data[1][-1]
                 del c_data[1]
-            if e_flag == (e_flag | 2**1):
+            if e_flag == (e_flag | 2 ** 1):
                 extended_result["rssi"] = int(c_data[1], base=16)
                 del c_data[1]
-            if e_flag == (e_flag | 2**2):
-                extended_result["time"] = int(c_data[1], base=16) + (int(c_data[2], base=16) << 4) + (int(c_data[3], base=16) << 8) + (int(c_data[4], base=16) << 12)
+            if e_flag == (e_flag | 2 ** 2):
+                extended_result["time"] = int(c_data[1], base=16) + (int(c_data[2], base=16) << 4) + (
+                            int(c_data[3], base=16) << 8) + (int(c_data[4], base=16) << 12)
                 del c_data[1:5]
-            if e_flag == (e_flag | 2**3):
-                extended_result["pc"] = int(c_data[1], base=16) + (int(c_data[2], base=16) << 4) + (int(c_data[3], base=16) << 8) + (int(c_data[4], base=16) << 12) + (int(c_data[5], base=16) << 16) + (int(c_data[6], base=16) << 20)
+            if e_flag == (e_flag | 2 ** 3):
+                extended_result["pc"] = int(c_data[1], base=16) + (int(c_data[2], base=16) << 4) + (
+                            int(c_data[3], base=16) << 8) + (int(c_data[4], base=16) << 12) + (
+                                                    int(c_data[5], base=16) << 16) + (int(c_data[6], base=16) << 20)
                 del c_data[1:7]
-            if e_flag == (e_flag | 2**4):
-                extended_result["frequency"] = int(c_data[1], base=16) + (int(c_data[2], base=16) << 4) + (int(c_data[3], base=16) << 8)
+            if e_flag == (e_flag | 2 ** 4):
+                extended_result["frequency"] = int(c_data[1], base=16) + (int(c_data[2], base=16) << 4) + (
+                            int(c_data[3], base=16) << 8)
                 del c_data[1:4]
                 extended_result["phase"] = int(c_data[1], base=16) + (int(c_data[2], base=16) << 4)
-                del c_data[1:3]                
+                del c_data[1:3]
             epc_bytes = int(c_data[1], base=16) * 2
             del c_data[1]
             epc = 0
-            for i in range(1, epc_bytes+1, 1):
-                epc = epc + (int(c_data[epc_bytes-(i-1)], base=16) << ((i-1) * 4))
-            extended_result["epc"] = epc                
+            for i in range(1, epc_bytes + 1, 1):
+                epc = epc + (int(c_data[epc_bytes - (i - 1)], base=16) << ((i - 1) * 4))
+            extended_result["epc"] = epc
             tag_list.append(extended_result)
             if c_data[1] == "0xaa" and c_data[2] == "0xcc":
                 break
     return tag_list
 
 
+root = Tk()
+root.title("KBRP V2 Test")
+
+s = socket.socket()
+buffer = 16384
+
+# Creating a Label Widget and a button
+reader_ip_label = Label(root, text="Reader IP:")
+reader_ip_entry = Entry(root, width=20)
+connect_button = Button(root, text="Connect", command=lambda: clickConnect(reader_ip_entry.get()))
+disconnect_button = Button(root, text="Disconnect", command=lambda: [s.close(), connection_status_label.config(
+    text="Disconnected!", fg='#ff0000')], state=DISABLED)
+connection_status_label = Label(root, text="")
+button_SyncGetEPCs = Button(root, text="SyncGetEPCs", command=SyncGetEPCs)
+label_SyncGetEPCs = Label(root, text="")
+
+# Shoving elements onto the screen
+reader_ip_label.grid(row=0, column=0, padx=10, pady=10)
+reader_ip_entry.grid(row=0, column=1, padx=10, pady=10)
+connect_button.grid(row=0, column=2, padx=10, pady=10)
+disconnect_button.grid(row=0, column=3, padx=10, pady=10)
+connection_status_label.grid(row=1, column=0, columnspan=4)
+button_SyncGetEPCs.grid(row=2, column=0, padx=10, pady=10)
+label_SyncGetEPCs.grid(row=3, column=0, columnspan=4)
+
+
 if __name__ == '__main__':
     main()
+
