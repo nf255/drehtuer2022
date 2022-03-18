ccc2129a1a1d82c9e7b897e22ec1b4e9a321e55f
nf255 <92917632+nf255@users.noreply.github.com>
2022-03-18 18:39:32 +0100

    Create GUI with tkinter
    - tkinter GUI created
    - execution of reader commands updated

--- TransferClient.py
+++ TransferClient.py
@@ -1,4 +1,6 @@
 import socket
+from tkinter import *
+
 
 # Dokumente:
 #
@@ -7,39 +9,64 @@ import socket
 
 
 def main():
-    reader = "192.168.3.46"
-    port = 4007
-    buffer = 16384
+    root = Tk()
+    root.title("KBRP V2 Test")
+
+    global s
     s = socket.socket()
+    buffer = 16384
+    connection_status = 1
+
+    # Creating a Label Widget and a button
+    global connection_status_label, connect_button, disconnect_button, button_SyncGetEPCs, label_SyncGetEPCs
+    reader_ip_label = Label(root, text="Reader IP:")
+    reader_ip_entry = Entry(root, width=20)
+    connect_button = Button(root, text="Connect", command=lambda: clickConnect(reader_ip_entry.get()))
+    disconnect_button = Button(root, text="Disconnect", command=lambda: [s.close(), connection_status_label.config(
+        text="Disconnected!", fg='#ff0000')], state=DISABLED)
+    connection_status_label = Label(root, text="")
+    button_SyncGetEPCs = Button(root, text="SyncGetEPCs", command=SyncGetEPCs, state=DISABLED)
+    label_SyncGetEPCs = Label(root, text="")
+
+    # Shoving elements onto the screen
+    reader_ip_label.grid(row=0, column=0, padx=10, pady=10)
+    reader_ip_entry.grid(row=0, column=1, padx=10, pady=10)
+    connect_button.grid(row=0, column=2, padx=10, pady=10)
+    disconnect_button.grid(row=0, column=3, padx=10, pady=10)
+    connection_status_label.grid(row=1, column=0, columnspan=4)
+    button_SyncGetEPCs.grid(row=2, column=0, padx=10, pady=10)
+    label_SyncGetEPCs.grid(row=3, column=0, columnspan=4)
+
+    root.mainloop()
+
+
+def clickConnect(reader):
     try:
-        s.connect((reader, port))
-        while True:
-            message = SyncGetEPCs()
-            s.send(message)
-            response = s.recv(buffer)
-            response_data = createResponseOutput(response)
-            print(response_data)
-            break
+        s.connect((reader, 4007))
     except:
-        print("Connection Failed!")
-    finally:
-        s.close()
-        print("Disconnected!")
+        connection_status_label.config(text="Connection failed!", fg='#ff0000')
+    else:
+        connection_status_label.config(text="Connected to " + reader + "!", fg='#00ff00')
+        disconnect_button.config(state=NORMAL)
+        button_SyncGetEPCs.config(state=NORMAL)
+
 
 def SyncGetEPCs():
     enc = [0xaa, 0xbb, 0x01, 0x01, 0x01, 0x01, 0xaa, 0xcc]
     message = bytes(enc)
-    return message
-        
+    s.send(message)
+    response = s.recv(buffer)
+    label_SyncGetEPCs.config(text=createResponseOutput(response))
+
 
 def createResponseOutput(response):
     handover = [hex(int(i)) for i in response]
     handover = handover[4:]
     command_id = hex(((int(handover[1], base=16) << 8) ^ (2 ** 15)) + int(handover[0], base=16))
-    command_data = handover[2:]   
+    command_data = handover[2:]
     command_res_dict = {
         "0x101": rSyncGetEPCs
-    }    
+    }
     r_command = command_res_dict.get(command_id)
     output = r_command(command_data)
     return output
