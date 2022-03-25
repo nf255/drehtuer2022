b1b86ebb63201f70c532ddc6b468a353f0f330c7
Nils Floß <N.Floss@KEMAS.de>
2022-03-25 14:45:14 +0100

    General Update

--- TransferClient.py
+++ TransferClient.py
@@ -1,8 +1,10 @@
 import socket
 from tkinter import *
+from tkinter import ttk
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+import statistics
 
 
 # Notes:
@@ -10,24 +12,30 @@ import time
 # S:\Entwicklung\Allgemein\Datenblätter\Kartentechnik\Kathrein\Programmierung\RRU4 API and Demo V2.56.00\API\Headers
 # S:\Entwicklung\Allgemein\Datenblätter\Kartentechnik\Kathrein\Programmierung
 # terminal: auto-py-to-exe
+# rssiThreshold
 
 
 def main():
-    reader_ip_label.grid(row=0, column=0, padx=8, pady=8)
-    reader_ip_entry.grid(row=0, column=1, padx=8, pady=8)
+    reader_ip_label.grid(row=0, column=0, padx=8, pady=16)
+    reader_ip_entry.grid(row=0, column=1, padx=8, pady=16)
     reader_ip_entry.insert(0, "192.168.3.46")
-    connect_button.grid(row=0, column=2, padx=8, pady=8)
-    disconnect_button.grid(row=0, column=3, padx=8, pady=8)
+    connect_button.grid(row=0, column=2, padx=8, pady=16)
+    disconnect_button.grid(row=0, column=3, padx=8, pady=16)
     connection_status_label.grid(row=1, column=0, columnspan=4)
-    button_SyncGetEPCs_increasing_power.grid(row=2, column=0, padx=8, pady=8)
-    button_SyncGetEPCs_timer.grid(row=3, column=0, rowspan=2, padx=8, pady=8)
+    button_SyncGetEPCs_rssi_timer.grid(row=3, column=0, padx=8, pady=8)
+    button_SyncGetEPCs_phase_timer.grid(row=4, column=0, padx=8, pady=8)
+    button_SyncGetEPCs_distance_timer.grid(row=6, column=0, padx=8, pady=8)
     timer_time_entry_label.grid(row=3, column=1, padx=8, pady=8)
     timer_time_entry.grid(row=4, column=1, padx=8, pady=8)
     timer_time_entry.insert(0, 10)
     timer_power_entry_label.grid(row=3, column=2, padx=8, pady=8)
     timer_power_entry.grid(row=4, column=2, padx=8, pady=8)
     timer_power_entry.insert(0, 20.0)
-    timer_alert_label.grid(row=5, column=0, columnspan=4, padx=8, pady=8)
+    timer_rssi_threshold_entry_label.grid(row=3, column=3, padx=8, pady=8)
+    timer_rssi_threshold_entry.grid(row=4, column=3, padx=8, pady=8)
+    timer_rssi_threshold_entry.insert(0, 50)
+    timer_alert_label.grid(row=6, column=1, columnspan=2, padx=8, pady=8)
+    button_SyncGetEPCs_increasing_power.grid(row=6, column=3, padx=8, pady=8)
     root.mainloop()
 
 
@@ -43,12 +51,36 @@ def clickConnect(reader):
         connect_button.config(state=DISABLED)
         disconnect_button.config(state=NORMAL)
         button_SyncGetEPCs_increasing_power.config(state=NORMAL)
-        button_SyncGetEPCs_timer.config(state=NORMAL)
+        button_SyncGetEPCs_rssi_timer.config(state=NORMAL)
+        button_SyncGetEPCs_phase_timer.config(state=NORMAL)
+        # button_SyncGetEPCs_distance_timer.config(state=NORMAL)
 
 
-def SyncGetEPCs(mode, timer_time, timer_power):
-    # enc = [0xaa, 0xbb, 0x01, 0x01, 0x19, 0x00, 0x17, 0xaa, 0xcc] # Extended Result Flag
+def SyncGetEPCs(mode, value_mode, timer_time, timer_power, timer_threshold):
+    try:
+        timer_time = float(timer_time)
+    except:
+        timer_alert_label.config(text="Invalid Time Input!")
+        return
+    try:
+        timer_power = round(float(timer_power) * 4) / 4
+    except:
+        timer_alert_label.config(text="Invalid Power Input!")
+        return
+    try:
+        timer_threshold = int(round(float(timer_threshold)))
+    except:
+        timer_alert_label.config(text="Invalid RSSI Input!")
+        return
+    enc = [0xaa, 0xbb, 0x01, 0x01, 0x19, 0x00, 0x17, 0xaa, 0xcc]  # Extended Result Flag
+    message = bytes(enc)
+    s.send(message)
+    placeholder = s.recv(buffer)
     if mode == "power":
+        enc = [0xaa, 0xbb, 0x01, 0x01, 0x02, 0x00, 0x00, 0xaa, 0xcc]  # SetMode (to NormalMode)
+        message = bytes(enc)
+        s.send(message)
+        placeholder = s.recv(buffer)
         plot_data = {}
         for i in range(24, 137):
             enc = [0xaa, 0xbb, 0x01, 0x01, 0x06, 0x00, 0x04, i, 0xaa, 0xcc]  # SetPortPower
@@ -82,48 +114,107 @@ def SyncGetEPCs(mode, timer_time, timer_power):
             message = bytes(enc)
             s.send(message)
             placeholder = s.recv(buffer)
-            process_time = time.perf_counter()
-            while time.perf_counter() - process_time < timer_time:
-                enc = [0xaa, 0xbb, 0x01, 0x01, 0x01, 0x01, 0xaa, 0xcc]  # SyncGetEPCs
+            if value_mode == "distance":
+                enc = [0xaa, 0xbb, 0x01, 0x01, 0x02, 0x00, 0x01, 0xaa, 0xcc]  # SetMode (to DirectMode)
                 message = bytes(enc)
                 s.send(message)
-                response = s.recv(buffer)
-                output_data = createResponseOutput(response)
+                placeholder = s.recv(buffer)
+                enc = [0xaa, 0xbb, 0x01, 0x01, 0x02, 0x02, 0x04, 0xaa, 0xcc]  # SetAntenna
+                message = bytes(enc)
+                s.send(message)
+                placeholder = s.recv(buffer)
+            process_time = time.perf_counter()
+            freq_switcher = 1
+            freq_cache = 0
+            max_phase = 0
+            while time.perf_counter() - process_time < timer_time:
+                freq_switcher = -freq_switcher
+                if value_mode == "distance":
+                    if freq_switcher > 0:
+                        enc = [0xaa, 0xbb, 0x01, 0x01, 0x03, 0x02, 0xa4, 0x35, 0x0d, 0xaa, 0xcc]  # SetFrequency (865.7)
+                    else:
+                        enc = [0xaa, 0xbb, 0x01, 0x01, 0x03, 0x02, 0x54, 0x3a, 0x0d, 0xaa, 0xcc]  # SetFrequency (866.9)
+                    message = bytes(enc)
+                    s.send(message)
+                    placeholder = s.recv(buffer)
+                    enc = [0xaa, 0xbb, 0x01, 0x01, 0x1e, 0x02, 0x64, 0x00, 0xaa, 0xcc]  # DirectBulkGetEPCs (100 ms)
+                    message = bytes(enc)
+                    s.send(message)
+                    response = s.recv(buffer)
+                    output_data = createResponseOutput(response)
+                else:
+                    enc = [0xaa, 0xbb, 0x01, 0x01, 0x01, 0x01, 0xaa, 0xcc]  # SyncGetEPCs
+                    message = bytes(enc)
+                    s.send(message)
+                    response = s.recv(buffer)
+                    output_data = createResponseOutput(response)
                 epc_out_list = []
                 for tag in output_data:
                     if tag["type"] == "tag":
                         if tag["epc"] not in plot_data:
-                            plot_data[tag["epc"]] = [[], []]
+                            plot_data[tag["epc"]] = [[], [], []]
                         plot_data[tag["epc"]][0].append(time.perf_counter() - process_time)
-                        plot_data[tag["epc"]][1].append(tag["rssi"])
+                        if value_mode != "distance":
+                            plot_data[tag["epc"]][1].append(tag[value_mode])
+                        else:
+                            plot_data[tag["epc"]][1].append(tag["phase"])
+                            plot_data[tag["epc"]][2].append(149896229 / tag["frequency"])
                         epc_out_list.append(tag["epc"])
-                for epc in plot_data:
-                    if epc not in epc_out_list:
-                        plot_data[epc][0].append(time.perf_counter() - process_time)
-                        plot_data[epc][1].append(0)
-                time.sleep(0.1)
-            for key, value in plot_data.items():
-                plt.plot(value[0], value[1], label=key[20:])
-            plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
-            plt.xlabel("Time in Seconds")
-            plt.ylabel("RSSI Value")
-            plt.title("Tag detection for " + str(timer_time) + " Seconds with " + str(timer_power) + " dBm")
-            plt.tight_layout()
-            plt.show()
+                        if freq_cache != tag["frequency"]:
+                            print(tag["frequency"])
+                        freq_cache = tag["frequency"]
+                        if tag["phase"] > max_phase:
+                            max_phase = tag["phase"]
+                # for epc in plot_data:
+                #     if epc not in epc_out_list:
+                #         plot_data[epc][0].append(time.perf_counter() - process_time)
+                #         plot_data[epc][1].append(0)
+            if value_mode != "distance":
+                for key, value in plot_data.items():
+                    plt.plot(value[0], value[1], label=key[20:])
+                plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
+                plt.xlabel("Time in Seconds")
+                plt.ylabel(value_mode.capitalize() + " Value")
+                plt.title("Tag detection for " + str(timer_time) + " Seconds with " + str(timer_power) + " dBm")
+                plt.tight_layout()
+                plt.show()
+            else:
+                for key, value in plot_data.items():
+                    timestamp_counter, time_1, time_2, time_3, time_4 = 0, 0, 0, 0, 0
+                    for timestamp in value[0]:
+                        timestamp_counter += 1
+                        if timestamp <= 1:
+                            time_1 = timestamp
+                        elif timestamp <= 3:
+                            time_2 = timestamp
+                        elif timestamp <= 5:
+                            time_3 = timestamp
+                        elif timestamp <= 7:
+                            time_4 = timestamp
+                    distance_phase_1 = statistics.mean(value[1][time_1:time_2])
+                    distance_phase_2 = statistics.mean(value[1][time_3:time_4])
+                    if value[2][time_1] < value[2][time_3]:
+                        pass
+            enc = [0xaa, 0xbb, 0x01, 0x01, 0x02, 0x00, 0x00, 0xaa, 0xcc]  # SetMode (to NormalMode)
+            message = bytes(enc)
+            s.send(message)
+            placeholder = s.recv(buffer)
+            print(max_phase)
         else:
             timer_alert_label.config(text="6 dBm <= Power <= 34 dBm!")
 
 
 def createResponseOutput(response):
-    handover = ["0x"+hex(int(i))[2:].zfill(2) for i in response]
+    handover = ["0x" + hex(int(i))[2:].zfill(2) for i in response]
     for count, value in enumerate(handover):
-        if value == "0xaa" and handover[count+1] == "0xaa":
+        if value == "0xaa" and handover[count + 1] == "0xaa":
             del handover[count]
     handover = handover[4:]
     command_id = hex(((int(handover[1], base=16) << 8) ^ (2 ** 15)) + int(handover[0], base=16))
     command_data = handover[2:]
     command_res_dict = {
-        "0x101": rSyncGetEPCs
+        "0x101": rSyncGetEPCs,
+        "0x21e": rSyncGetEPCs
     }
     r_command = command_res_dict.get(command_id)
     output = r_command(command_data)
@@ -135,7 +226,7 @@ def rSyncGetEPCs(c_data):
     info = {"type": "info", "command": "SyncGetEPCs"}
     if c_data[0] == "0x00":
         info["error"] = "none"
-    elif c_data[0] == "0x0A":
+    elif c_data[0] == "0x0a":
         info["error"] = "no_tag"
     else:
         info["error"] = c_data[0]
@@ -166,7 +257,7 @@ def rSyncGetEPCs(c_data):
                 extended_result["frequency"] = int(c_data[1], base=16) + (int(c_data[2], base=16) << 8) + (
                         int(c_data[3], base=16) << 16)
                 del c_data[1:4]
-                extended_result["phase"] = int(c_data[1], base=16) + (int(c_data[2], base=16) << 8)
+                extended_result["phase"] = (int(c_data[1], base=16) + (int(c_data[2], base=16) << 8)) / 10 - 90
                 del c_data[1:3]
             epc_bytes = int(c_data[1], base=16) * 2
             del c_data[1]
@@ -185,7 +276,6 @@ root.title("KBRP V2 Test")
 s = socket.socket()
 buffer = 16384
 
-# Creating a Label Widget and a button
 reader_ip_label = Label(root, text="Reader IP:")
 reader_ip_entry = Entry(root, width=20)
 connect_button = Button(root, text="Connect", command=lambda: clickConnect(reader_ip_entry.get()))
@@ -196,24 +286,48 @@ disconnect_button = Button(root,
                                             disconnect_button.config(state=DISABLED),
                                             connect_button.config(state=NORMAL),
                                             button_SyncGetEPCs_increasing_power.config(state=DISABLED),
-                                            button_SyncGetEPCs_timer.config(state=DISABLED)],
+                                            button_SyncGetEPCs_rssi_timer.config(state=DISABLED),
+                                            button_SyncGetEPCs_phase_timer.config(state=DISABLED),
+                                            button_SyncGetEPCs_distance_timer.config(state=DISABLED)],
                            state=DISABLED)
 connection_status_label = Label(root, text="")
-button_SyncGetEPCs_increasing_power = Button(root,
-                                             text="Increasing Power",
-                                             command=lambda: SyncGetEPCs("power", 0, 0),
-                                             state=DISABLED)
-button_SyncGetEPCs_timer = Button(root,
-                                  text="Timer",
-                                  command=lambda: SyncGetEPCs("timer",
-                                                              int(timer_time_entry.get()),
-                                                              round(float(timer_power_entry.get()) * 4) / 4),
-                                  state=DISABLED)
+ttk.Separator(root, orient=HORIZONTAL).grid(row=2, column=0, columnspan=5, sticky='ew')
+button_SyncGetEPCs_rssi_timer = Button(root,
+                                       text="RSSI Determination",
+                                       command=lambda: SyncGetEPCs("timer",
+                                                                   "rssi",
+                                                                   timer_time_entry.get(),
+                                                                   timer_power_entry.get(),
+                                                                   timer_rssi_threshold_entry.get()),
+                                       state=DISABLED)
+button_SyncGetEPCs_phase_timer = Button(root,
+                                        text="Phase Determination",
+                                        command=lambda: SyncGetEPCs("timer",
+                                                                    "phase",
+                                                                    timer_time_entry.get(),
+                                                                    timer_power_entry.get(),
+                                                                    timer_rssi_threshold_entry.get()),
+                                        state=DISABLED)
+button_SyncGetEPCs_distance_timer = Button(root,
+                                           text="Distance Determination",
+                                           command=lambda: [SyncGetEPCs("timer",
+                                                                        "distance",
+                                                                        timer_time_entry.get(),
+                                                                        timer_power_entry.get(),
+                                                                        timer_rssi_threshold_entry.get())],
+                                           state=DISABLED)
 timer_time_entry_label = Label(root, text="Time (s):")
 timer_time_entry = Entry(root, width=8)
 timer_power_entry_label = Label(root, text="Power (dBm):")
 timer_power_entry = Entry(root, width=8)
+timer_rssi_threshold_entry_label = Label(root, text="RSSI Threshold:")
+timer_rssi_threshold_entry = Entry(root, width=8)
 timer_alert_label = Label(root, text="", fg='#ff0000')
+button_SyncGetEPCs_increasing_power = Button(root,
+                                             text="Increasing Power Mode",
+                                             command=lambda: SyncGetEPCs("power", "", 0, 0,
+                                                                         timer_rssi_threshold_entry.get()),
+                                             state=DISABLED)
 
 
 if __name__ == '__main__':
